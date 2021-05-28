417318404259e2c201105e83194424de99ef801b
kafkaphoenix <jaguilerapuerta@gmail.com>
2021-05-29 00:50:27 +0200

    E6-E7
    Added Biomes
    Added ocean depth

--- 3d world generation/Assets/Scripts/Planet.cs
+++ 3d world generation/Assets/Scripts/Planet.cs
@@ -64,7 +64,8 @@ public class Planet : MonoBehaviour
 
     public void OnShapeSettingsUpdated()
     {
-        if (autoUpdate == true) {      
+        if (autoUpdate)
+        {      
             Initialize();
             GenerateMesh();
         }
@@ -72,7 +73,8 @@ public class Planet : MonoBehaviour
 
     public void OnColourSettingsUpdated()
     {
-        if (autoUpdate == true) {
+        if (autoUpdate)
+        {
             Initialize();
             GenerateColours();
         }
@@ -94,5 +96,12 @@ public class Planet : MonoBehaviour
     void GenerateColours()
     {
         colourGenerator.UpdateColours();
+         for (int i = 0; i < 6; i++)
+        {
+            if (meshFilters[i].gameObject.activeSelf)
+            {
+                terrainFaces[i].UpdateUVs(colourGenerator);
+            }
+        }
     }
 }
